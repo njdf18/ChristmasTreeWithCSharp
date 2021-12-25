9ce32276690b78a6d33b5a06ffb68afa7a54cfdc
Number18 <86307226+njdf18@users.noreply.github.com>
2021-12-25 12:14:53 +1000

    ver 1.00
    Just a undecorated tree, maybe I can add sth next year...?

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = 10;
+
+            Console.Write("Merry Christmas! This is a undecorated christmas tree\n");
+
+            for (int l = 0; l <= 1; l++)
+            {
+                for (int i = 1; i < 10; i++)     //height
+                {
+                    for (int j = 0; j < v; j++)    //print space, get lesser
+                        Console.Write(" ");
+
+                    v -= 1;
+
+                    for (int t = 1; t < i * 2; t++)    //print stars in odd numbers
+                        Console.Write("*");
+
+                    Console.WriteLine();    //next line
+                }
+                v = 10;
+            }
+
+            for(int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 9; j++)    //print space, get lesser
+                    Console.Write(" ");
+
+                for (int t = 0; t < 3; t++)    //print stars in odd numbers
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
